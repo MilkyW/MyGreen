f9dc2dbbfe6d9137d547eb22b3b593053e28a946
Milky.W <minniewu@qq.com>
2018-07-02 17:18:36 +0800

    tried Unity button
    to change the attributes of a Sprite

---
+++ MyGreen/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    SpriteRenderer m_SpriteRenderer;
+
+    // Use this for initialization
+    void Start () {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SpriteRenderer.color = Color.white;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void ChangeColor()
+    {
+        if (m_SpriteRenderer.color == Color.white)
+        {
+            m_SpriteRenderer.color = Color.red;
+        }
+
+        else if (m_SpriteRenderer.color == Color.red)
+        {
+            m_SpriteRenderer.color = Color.white;
+        }
+    }
+}
