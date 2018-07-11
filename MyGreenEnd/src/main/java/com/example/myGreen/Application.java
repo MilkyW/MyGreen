2975dcaa635f83bca5dad8fc77941e640a39ece6
black197 <1276704961@qq.com>
2018-07-11 10:35:45 +0800

    prepare for pull request
    1、删除不必要的内容，只保留了发送邮件与验证链接的功能

--- MyGreenEnd/src/main/java/com/example/myGreen/Application.java
+++ MyGreenEnd/src/main/java/com/example/myGreen/Application.java
@@ -2,11 +2,13 @@ package com.example.myGreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext appContext = SpringApplication.run(Application.class, args);
+        SpringUtil.setApplicationContext(appContext);
     }
 }
