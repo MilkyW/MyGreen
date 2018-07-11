c506e3b5f97fc59c32519e9f152b204eb4ce76c5
Milky.W <minniewu@qq.com>
2018-07-11 22:48:04 +0800

    render sensors or controller using C#

--- code/MyGreen/Assets/Scripts/MapBG.cs
+++ code/MyGreen/Assets/Scripts/MapBG.cs
@@ -2,15 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapBG : MonoBehaviour {
+public class MapBG : MonoBehaviour
+{
+    public enum SensorControllerType
+    {
+        Temperature,
+        Moisture,
+        Irrigation
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void letUsDraw()
+    {
+        drawOne(12345, "ME", 111, 112, SensorControllerType.Temperature, true);
+    }
+
+    public void drawOne(long id, string name, int x, int y, SensorControllerType type, bool valid)
+    {
+        Object sensorcontrollerPreb = null;
+        GameObject sensorcontroller;
+        GameObject container = GameObject.Find("Map");
+
+        switch (type)
+        {
+            case SensorControllerType.Temperature:
+                sensorcontrollerPreb = Resources.Load("Sensors/TemperatureSensor", typeof(GameObject));
+                break;
+
+            case SensorControllerType.Moisture:
+                sensorcontrollerPreb = Resources.Load("Sensors/MoistureSensor", typeof(GameObject));
+                break;
+
+            case SensorControllerType.Irrigation:
+                sensorcontrollerPreb = Resources.Load("Controllers/IrrigationController", typeof(GameObject));
+                break;
+
+            default:
+                return;
+        }
+
+        if (sensorcontrollerPreb == null)
+            return;
+        sensorcontroller = Instantiate(sensorcontrollerPreb) as GameObject;
+        SensorController sc = sensorcontroller.GetComponent<SensorController>();
+        sc.setID(id);
+        sc.setName(name);
+        sc.setValid(valid);
+        sc.transform.SetParent(container.transform);
+        RectTransform rt = sensorcontroller.GetComponent<RectTransform>();
+        rt.anchoredPosition = new Vector2(x, y);
+        rt.localScale = new Vector3(1, 1, 1);
+    }
 }
--- code/MyGreen/Assets/Scripts/SensorController.cs
+++ code/MyGreen/Assets/Scripts/SensorController.cs
@@ -7,14 +7,17 @@ using UnityEngine.UI;
 public class SensorController : MonoBehaviour {
 
     private bool showName;
+    private long id;
     private string name;
+    private bool valid;
     private GameObject textObj;
     private Text text;
 
 	// Use this for initialization
 	void Start () {
         showName = false;
-        name = "My Name";
+        if (name == null)
+            name = "My Name";
         textObj = this.gameObject.transform.GetChild(1).gameObject;
         textObj.SetActive(false);
         text = textObj.GetComponent<Text>();
@@ -25,6 +28,24 @@ public class SensorController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void setID(long i)
+    {
+        id = i;
+        return;
+    }
+
+    public void setName(string n)
+    {
+        name = n;
+        return;
+    }
+
+    public void setValid(bool v)
+    {
+        valid = v;
+        return;
+    }
 
     public void selected()
     {
